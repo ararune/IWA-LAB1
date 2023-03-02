4986792d72d2397bc7f63f04af64a2489827e08a
Josip Čondić Jurkić <51995137+ararune@users.noreply.github.com>
2023-03-02 20:54:57 +0100

    Update crawler.py

--- IWA/crawler.py
+++ IWA/crawler.py
@@ -1,31 +1,35 @@
-import socket
-import time
-import re
+import socket, time
 
-def connect_to_server(url):
-    url = url.strip("http://").strip("https://").strip("/")
-    ip = socket.gethostbyname(url)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((ip, 80))
+def connect_to_server(homepage, port, retry = 5):
+    s = socket.socket()
+    try:
+        s.connect((homepage, port))
+    except Exception as e:
+        print (e)
+        if retry > 0:
+            time.sleep(1)
+            retry -=1
+            connect_to_server(homepage, port, retry)       
+    
     return s
 
-def get_source(s, url):
-    CRLF = "\r\n"
-    get = f"GET / HTTP/1.1{CRLF}"
-    get += f"Host: {url}{CRLF}"
-    get += f"Connection: close{CRLF}"
+def strip_url(url):
+    return url.strip("http://").strip("https://").strip("/")
+
+def get_source(s, homepage, endpoint):
+
+    CRLF = '\r\n'
+    get = 'GET /' + endpoint + ' HTTP/1.1' + CRLF
+    get += 'Host: '
+    get += homepage
+    get += CRLF
     get += CRLF
 
-    s.send(get.encode("utf-8"))
-    response = b""
-    while True:
-        chunk = s.recv(4096)
-        if not chunk:
-            break
-        response += chunk
-    return response.decode("utf-8")
+    s.send(get.encode('utf-8'))
+    response = s.recv(10000000).decode('latin-1')
+    return response
 
-def get_links(response):
+def get_all_hrefs(response, homepage):
     links = []
     while True:
         start = response.find("<a ")
@@ -35,15 +39,44 @@ def get_links(response):
         href = response[start:end].split("href=")[-1].split()[0].strip("\"'")
 
         if href not in links:
-            links.append(href)
-
+            if href.startswith("http") or href.startswith("https"):
+                href = strip_url(href)
+                links.append(href)
+            else:
+                href = homepage + href
+                links.append(href)
+                
         response = response[end:]
+
     return links
 
-if __name__ == "__main__":
-    url = "https://crawler-test.com/"
-    s = connect_to_server(url)
-    response = get_source(s, url)
-    links = get_links(response)
-    print(links)
-    s.close()
+homepage = 'https://crawler-test.com/'
+port = 80
+endpoint = ''
+
+homepage = strip_url(homepage)
+s = connect_to_server(homepage, port)
+response = get_source(s, homepage, endpoint)
+
+links = get_all_hrefs(response, homepage)
+links.pop(0)
+print(len(links))
+
+counter = 0
+while counter < 10:
+    new_links = []
+    for link in links:
+        try:
+            response = get_source(s, homepage, endpoint)
+            if "200 OK" in response:
+                counter += 1
+                new_links.extend(get_all_hrefs(response, link))
+        except Exception as e:
+            print(f"Error visiting link {link}: {e}")
+        if counter >= 10:
+            break
+    links.extend(new_links)
+
+print(links)
+print(len(links))
+
